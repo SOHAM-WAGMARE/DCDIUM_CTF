c5e377b90af6f288a7d0a1ad412dcd4e9183fe00
SOHAM-WAGMARE <157987245+SOHAM-WAGMARE@users.noreply.github.com>
2024-01-29 00:13:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showDropdown() {
+    var dropdownContent = document.getElementById('resources-dropdown');
+    dropdownContent.style.display = 'block';
+}
+
+function hideDropdown() {
+    var dropdownContent = document.getElementById('resources-dropdown');
+    dropdownContent.style.display = 'none';
+}
